8e250551f4743265340e6cca610302b084827315
blur64 <shalomsi922@gmail.com>
2024-07-05 13:31:35 +0400

    add validations and update namings in Timer.js

--- src/models/Timer.js
+++ src/models/Timer.js
@@ -7,31 +7,102 @@ const timerStates = {
 
 /**
  * Model representing a timer with its own tick mechanism.
- * @todo Add checks in methods. For example, remainingMilliseconds() returns
+ * @todo Add checks in methods. For example, _remainingMilliseconds() returns
  * a value even if the timer is finished or didn't started.
  */
 export default class Timer {
   _initialDuration = 0;
-  _duration = 0;
+  _millisecondsLeftToWork = 0;
   _launchTime = 0;
   _timeoutId = 0;
   _baseTickInterval = 1000;
   _state = timerStates.READY;
 
-  _onSecondTick () {}
-  _onFinish () {}
+  _onSecondTick = null;
+  _onFinish = null;
 
-  constructor({ duration: { seconds, minutes }, onSecondTick, onFinish }) {
-    this._initialDuration = minutes * 60 * 1000 + seconds * 1000;
-    this._duration = this._initialDuration;
-    this._onSecondTick = onSecondTick;
-    this._onFinish = onFinish;
+  constructor({ duration, onSecondTick, onFinish }) {
+    this._validateParams({ duration, onSecondTick, onFinish });
+
+    this.setDuration(duration);
+    this._onSecondTick = onSecondTick ?? function () {};
+    this._onFinish = onFinish ?? function () {};
   }
 
-  setDuration({ seconds, minutes }) {
-    this._initialDuration = minutes * 60 * 1000 + seconds * 1000;
-    this._duration = this._initialDuration;
+  get _remainingMilliseconds() {
+    return this.isActive
+      ? this._millisecondsLeftToWork - this._passedMillisecondsFromLaunchMoment
+      : this._millisecondsLeftToWork;
   }
+
+  get _passedMillisecondsFromLaunchMoment() {
+    return Date.now() - this._launchTime;
+  }
+
+  _setState(state) {
+    this._checkState(state);
+
+    this._state = state;
+  }
+
+  _checkState(state) {
+    if (state && Object.values(timerStates).includes(state)) { 
+      return;
+    }
+
+    throw new Error('Incorrect timer state name');
+  }
+
+  _validateParams({ duration, onSecondTick, onFinish }) {
+    if (duration.seconds < 0 || duration.minutes < 0) {
+      throw new Error('Provided duration should be more than zero');
+    }
+
+    if (onSecondTick !== undefined && !(onSecondTick instanceof Function)
+        || onFinish !== undefined && !(onFinish instanceof Function)) {
+      throw new TypeError('Provided callbacks are not functions');
+    }
+  }
+
+  _extractTimeParts(milliseconds) {
+    const roundedMilliseconds = Math.round(milliseconds / 1000) * 1000;
+    const time = new Date(roundedMilliseconds);
+
+    return {
+      seconds: time.getSeconds(),
+      minutes: time.getMinutes()
+    };
+  }
+
+  _tick() {
+    if (this._remainingMilliseconds <= 0) {
+      this._onSecondTick(this._extractTimeParts(0));
+      this.finish();
+      return;
+    }
+
+    this._onSecondTick(this._extractTimeParts(this._remainingMilliseconds));
+    this._timeoutId = setTimeout(() => this._tick(), this._calcTickInterval());
+  }
+
+  _calcTickInterval() {
+    const drift = this._passedMillisecondsFromLaunchMoment % this._baseTickInterval;
+    const tickIntervalAffectedByDrift = this._baseTickInterval - drift;
+
+    return tickIntervalAffectedByDrift;
+  }
+
+  _stopTicking() {
+    clearTimeout(this._timeoutId);
+    this._timeoutId = 0;
+  }
+
+  _launch() {
+    this._launchTime = Date.now();
+    this._timeoutId = setTimeout(() => this._tick(), this._baseTickInterval);
+  }
+
+  /** Public */
 
   get isFinished() {
     return this._state === timerStates.FINISHED;
@@ -49,84 +120,21 @@ export default class Timer {
     return this._state === timerStates.STOPPED;
   }
 
-  get duration() {
-    return this._duration;
-  }
-
-  /**
-   * Calculate and returns the time after which the timer will be stopped.
-   * If timer is stopped or hasn't been started yet, returns 
-   * @returns {number} Time in milliseconds
-   */
-  get remainingMilliseconds() {
-    return this.isActive ?
-      this._duration - this._calcPassedMilliseconds() :
-      this._duration;
-  }
-
-  _calcPassedMilliseconds() {
-    return Date.now() - this._launchTime;
-  }
-
-  _tick() {
-    if (this.remainingMilliseconds <= 0) {
-      this._onSecondTick(this._extractTimeParts(0));
-      this.finish();
-      return;
-    }
-    this._onSecondTick(this._extractTimeParts(this.remainingMilliseconds));
-    this._timeoutId = setTimeout(() => this._tick(), this._calcTickInterval());
-  }
-
-  _calcTickInterval() {
-    const drift = this._calcPassedMilliseconds() % this._baseTickInterval;
-    const tickIntervalAffectedByDrift = this._baseTickInterval - drift;
-    return tickIntervalAffectedByDrift;
-  }
-
-  _extractTimeParts(milliseconds) {
-    const roundedMilliseconds = Math.round(milliseconds / 1000) * 1000;
-    const time = new Date(roundedMilliseconds);
-    return {
-      seconds: time.getSeconds(),
-      minutes: time.getMinutes()
-    };
-  }
-
-  _stopTicking() {
-    clearTimeout(this._timeoutId);
-    this._timeoutId = 0;
-  }
-
-  _launch() {
-    this._launchTime = Date.now();
-    this._timeoutId = setTimeout(() => this._tick(), this._baseTickInterval);
-  }
-
-  _checkState(state) {
-    if (state && Object.values(timerStates).includes(state)) { 
-      return;
-    }
-
-    throw new Error('Incorrect timer state name');
-  }
-
-  _setState(state) {
-    this._checkState(state);
-
-    this._state = state;
+  setDuration({ seconds, minutes }) {
+    this._initialDuration = minutes * 60 * 1000 + seconds * 1000;
+    this._millisecondsLeftToWork = this._initialDuration;
   }
 
   start() {
     if (this.isReady) {
       this._launch();
-      this._setState(timerStates.ACTIVE);
+      this._setState(timerStates.ACTIVE); 
     }
   }
 
   stop() {
     if (this.isActive) {
-      this._duration = Math.ceil(this.remainingMilliseconds / 1000) * 1000;
+      this._millisecondsLeftToWork = Math.ceil(this._remainingMilliseconds / 1000) * 1000;
       this._stopTicking();
       this._setState(timerStates.STOPPED);
     }
@@ -148,7 +156,7 @@ export default class Timer {
       this._stopTicking(); 
     }
 
-    this._duration = this._initialDuration;
+    this._millisecondsLeftToWork = this._initialDuration;
     this._setState(timerStates.READY);
   }
 
