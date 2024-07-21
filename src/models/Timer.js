2f71ecb221238133e70add5b0bb01a666d80b451
blur64 <shalomsi922@gmail.com>
2024-07-21 12:02:59 +0400

    change timer properties names

--- src/models/Timer.js
+++ src/models/Timer.js
@@ -5,35 +5,28 @@ const timerStates = {
   FINISHED: "finished",
 };
 
-/**
- * Model representing a timer with its own tick mechanism.
- * @todo Add checks in methods. For example, _remainingMilliseconds() returns
- * a value even if the timer is finished or didn't started.
- */
 export default class Timer {
-  _duration = 0;
-  _millisecondsLeftToWork = 0;
+  _allDuration = 0;
+  _remainingDuration = 0;
   _launchTime = 0;
   _timeoutId = 0;
   _baseTickInterval = 1000;
   _state = timerStates.READY;
-  
+
   _onSecondTick = null;
   _onFinish = null;
 
-  constructor({ duration, onSecondTick, onFinish }) {
+  constructor({ duration, onSecondTick = () => {}, onFinish = () => {} }) {
     this._validateParams({ duration, onSecondTick, onFinish });
 
-    this._duration = duration.minutes * 60 * 1000 + duration.seconds * 1000;
-    this._millisecondsLeftToWork = this._duration;
-    this._onSecondTick = onSecondTick ?? function () {};
-    this._onFinish = onFinish ?? function () {};
+    this._allDuration = duration.minutes * 60 * 1000 + duration.seconds * 1000;
+    this._remainingDuration = this._allDuration;
+    this._onSecondTick = onSecondTick;
+    this._onFinish = onFinish;
   }
 
-  get _remainingMilliseconds() {
-    return this.isActive
-      ? this._millisecondsLeftToWork - this._passedMillisecondsFromLaunchMoment
-      : this._millisecondsLeftToWork;
+  get _millisecondsLeftToWork() {
+    return this._remainingDuration - this._passedMillisecondsFromLaunchMoment;
   }
 
   get _passedMillisecondsFromLaunchMoment() {
@@ -58,17 +51,24 @@ export default class Timer {
 
   _validateParams({ duration, onSecondTick, onFinish }) {
     if (duration.seconds < 0 || duration.minutes < 0) {
-      throw new Error('Provided duration should be more than zero');
+      throw new Error("Provided duration should be more than zero");
     }
 
-    if (onSecondTick !== undefined && !(onSecondTick instanceof Function)
-        || onFinish !== undefined && !(onFinish instanceof Function)) {
-      throw new TypeError('Provided callbacks are not functions');
+    if (!(onSecondTick instanceof Function && onFinish instanceof Function)) {
+      throw new TypeError("Provided callbacks are not functions");
     }
   }
 
+  _ceilTimestampMilliseconds(timestamp) {
+    return Math.ceil(timestamp / 1000) * 1000;
+  }
+
+  _roundTimestampMilliseconds(timestamp) {
+    return Math.round(timestamp / 1000) * 1000;
+  }
+
   _extractTimeParts(milliseconds) {
-    const roundedMilliseconds = Math.round(milliseconds / 1000) * 1000;
+    const roundedMilliseconds = this._roundTimestampMilliseconds(milliseconds);
     const time = new Date(roundedMilliseconds);
 
     return {
@@ -78,13 +78,14 @@ export default class Timer {
   }
 
   _tick() {
-    if (this._remainingMilliseconds <= 0) {
+    if (this._millisecondsLeftToWork <= 0) {
       this._onSecondTick(this._extractTimeParts(0));
       this.finish();
+
       return;
     }
 
-    this._onSecondTick(this._extractTimeParts(this._remainingMilliseconds));
+    this._onSecondTick(this._extractTimeParts(this._millisecondsLeftToWork));
     this._timeoutId = setTimeout(() => this._tick(), this._calcTickInterval());
   }
 
@@ -132,7 +133,7 @@ export default class Timer {
 
   stop() {
     if (this.isActive) {
-      this._millisecondsLeftToWork = Math.ceil(this._remainingMilliseconds / 1000) * 1000;
+      this._remainingDuration = this._ceilTimestampMilliseconds(this._millisecondsLeftToWork);
       this._stopTicking();
       this._toStopped();
     }
@@ -146,15 +147,15 @@ export default class Timer {
   }
 
   reset() {
-    if (this.isReady) { 
-      return; 
+    if (this.isReady) {
+      return;
     }
 
-    if (!this.isFinished) { 
-      this._stopTicking(); 
+    if (!this.isFinished) {
+      this._stopTicking();
     }
 
-    this._millisecondsLeftToWork = this._duration;
+    this._remainingDuration = this._allDuration;
     this._toReady();
   }
 
